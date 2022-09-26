13278a9b0fcb56a2e7b15e5939d09f43d0b3d7d6
HoangDr <drhoang999@gmail.com>
2022-09-26 17:00:13 +0700

    Bonus point agency

--- src/components/RewardPoint/Form.js
+++ src/components/RewardPoint/Form.js
@@ -27,46 +27,47 @@ class Form extends Component {
       ex: 100000,
       percent_use_max_point: "",
       is_percent_use_max_point: false,
+      bonus_point_bonus_product_to_agency: false,
+      bonus_point_product_to_agency: false
     };
   }
   onChange = (e) => {
     var target = e.target;
     var name = target.name;
     var value = target.value;
-    if(name=="percent_use_max_point")
-    {
-      if((parseInt(value) || 0) < 100)
-      this.setState({ [name]: value });
+    if (name == "percent_use_max_point") {
+      if ((parseInt(value) || 0) < 100)
+        this.setState({ [name]: value });
       else
-      return;
+        return;
     }
     const valueFormat = formatNumber(value);
     console.log(valueFormat)
     if (!isNaN(Number(valueFormat))) {
       if (name == "percent_refund") {
         if (parseInt(valueFormat) < 100) {
-          if(valueFormat === 0)
-          this.setState({ [name]: "" });
+          if (valueFormat === 0)
+            this.setState({ [name]: "" });
           else
-          this.setState({ [name]: valueFormat });
+            this.setState({ [name]: valueFormat });
 
 
         }
       } else if (name == "percent_use_max_point") {
         if (parseInt(valueFormat) < 100) {
-          if(valueFormat === 0)
-          this.setState({ [name]: "" });
+          if (valueFormat === 0)
+            this.setState({ [name]: "" });
           else
-          this.setState({ [name]: valueFormat });
+            this.setState({ [name]: valueFormat });
 
 
         }
       }
       else {
-        if(value == "")
-        this.setState({ [name]: "" });
+        if (value == "")
+          this.setState({ [name]: "" });
         else
-        this.setState({ [name]: valueFormat });
+          this.setState({ [name]: valueFormat });
 
 
       }
@@ -101,6 +102,8 @@ class Form extends Component {
       point_review: rewardPoints.point_review,
       is_percent_use_max_point: rewardPoints.is_percent_use_max_point,
       percent_use_max_point: rewardPoints.percent_use_max_point,
+      bonus_point_bonus_product_to_agency: rewardPoints.bonus_point_bonus_product_to_agency,
+      bonus_point_product_to_agency: rewardPoints.bonus_point_product_to_agency,
     })
   }
 
@@ -125,10 +128,13 @@ class Form extends Component {
       point_introduce_customer,
       percent_use_max_point,
       is_percent_use_max_point,
+      bonus_point_product_to_agency,
+      bonus_point_bonus_product_to_agency,
       point_review } = this.state
     var form = {
       allow_use_point_order,
-      is_set_order_max_point,
+      is_set_order_max_point, bonus_point_product_to_agency,
+      bonus_point_bonus_product_to_agency,
       money_a_point: money_a_point == null ? money_a_point : money_a_point.toString().replace(/,/g, '').replace(/-/g, ''),
       order_max_point: order_max_point == null ? order_max_point : order_max_point.toString().replace(/,/g, '').replace(/-/g, ''),
       percent_refund: percent_refund == null ? percent_refund : percent_refund.toString().replace(/,/g, '').replace(/-/g, ''),
@@ -159,7 +165,7 @@ class Form extends Component {
       point_introduce_customer,
       is_percent_use_max_point,
       percent_use_max_point,
-      point_review, ex } = this.state
+      point_review, ex, bonus_point_product_to_agency, bonus_point_bonus_product_to_agency } = this.state
     var checkedAllow = allow_use_point_order == true ? true : false
     var checkMaxPoint = is_set_order_max_point == true ? true : false
     var disableValueMaxPoint = is_set_order_max_point == true ? "show" : "hide"
@@ -167,23 +173,25 @@ class Form extends Component {
     var isPercentUseMaxPoint = is_percent_use_max_point == true ? true : false
     var disablePercentUseMaxPont = is_percent_use_max_point == true ? "show" : "hide"
 
+
+
     return (
       <React.Fragment>
         <form role="form" onSubmit={this.onSave} >
           <div class="box-body">
             <div className="box-body">
-         
-              
-                <React.Fragment>
+
+
+              <React.Fragment>
                 <div className="form-group">
                   <label htmlFor="lname">Quy đổi xu thành VNĐ</label>
                   <div className="group" style={{ display: "flex" }}>
                     <div style={{ display: "flex" }}>
                       <span style={{ margin: "auto", minWidth: "100px " }}>1 Xu bằng: </span>
-  
+
                     </div>
                     <div class="input-group" style={{ marginLeft: "20px", maxWidth: "100%" }}>
-  
+
                       <input type="text" class="form-control" onChange={this.onChange} name="money_a_point" value={formatNoDWithEmpty(money_a_point)} placeholder="Nhập số tiền..." />
                       <div class="input-group-append">
                         <span class="input-group-text" id="basic-addon2">VNĐ</span>
@@ -191,22 +199,22 @@ class Form extends Component {
                     </div>
                   </div>
                 </div>
-  
+
                 <div className="form-group">
                   <label htmlFor="phone">Phần trăm hoàn xu cho mỗi đơn hàng</label>
                   <div className="group" style={{ display: "flex" }}>
                     <div style={{ display: "flex" }}>
                       <span style={{ margin: "auto", minWidth: "100px " }}>Phần trăm: </span>
-  
+
                     </div>
                     <div class="input-group" style={{ marginLeft: "20px", maxWidth: "100%" }}>
-  
+
                       <input type="text" class="form-control" value={percent_refund} onChange={this.onChange} name="percent_refund" placeholder="Nhập phần trăm..." />
                       <div class="input-group-append">
                         <span class="input-group-text" id="basic-addon2">%</span>
                       </div>
                     </div>
-  
+
                   </div>
                   <p
                     style={{ paddingTop: "10px" }}
@@ -219,18 +227,18 @@ class Form extends Component {
                     </i>
                   </p>
                 </div>
-  
-  
-  
+
+
+
                 <div className="form-group">
                   <label htmlFor="phone">Số xu nhận được khi đánh giá sản phẩm</label>
                   <div className="group" style={{ display: "flex" }}>
                     <div style={{ display: "flex" }}>
                       <span style={{ margin: "auto", minWidth: "100px " }}>Số Xu: </span>
-  
+
                     </div>
                     <div class="input-group" style={{ marginLeft: "20px", maxWidth: "100%" }}>
-  
+
                       <input type="text" class="form-control" onChange={this.onChange} value={formatNoDWithEmpty(point_review)} name="point_review" placeholder="Nhập số xu..." />
                       <div class="input-group-append">
                         <span class="input-group-text" id="basic-addon2">Xu</span>
@@ -238,18 +246,18 @@ class Form extends Component {
                     </div>
                   </div>
                 </div>
-  
-  
-  
+
+
+
                 <div className="form-group">
                   <label htmlFor="phone">Số xu nhận được khi Giới thiệu 'Được' bạn bè</label>
                   <div className="group" style={{ display: "flex" }}>
                     <div style={{ display: "flex" }}>
                       <span style={{ margin: "auto", minWidth: "100px " }}>Số Xu: </span>
-  
+
                     </div>
                     <div class="input-group" style={{ marginLeft: "20px", maxWidth: "100%" }}>
-  
+
                       <input type="text" class="form-control" value={formatNoDWithEmpty(point_introduce_customer)} onChange={this.onChange} name="point_introduce_customer" placeholder="Nhập số xu..." />
                       <div class="input-group-append">
                         <span class="input-group-text" id="basic-addon2">Xu</span>
@@ -257,81 +265,104 @@ class Form extends Component {
                     </div>
                   </div>
                 </div>
-  
-          
-         
+
+
+
                 <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" checked={checkedAllow} name="allow_use_point_order" onChange={this.onChangeCheckBox} id="gridCheck" />
-                  <label class="form-check-label" for="gridCheck">
-                    Cho phép sử dụng xu khi mua hàng
-                  </label>
+                  <label htmlFor="phone">Sử dụng xu mua hàng</label>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" checked={checkedAllow} name="allow_use_point_order" onChange={this.onChangeCheckBox} id="gridCheck33" />
+                    <label class="form-check-label" for="gridCheck33">
+                      Cho phép sử dụng xu khi mua hàng
+                    </label>
+                  </div>
+
+                </div>
+                {
+                  checkedAllow == true &&
+                  <>
+                    <div class="form-group">
+                      <div class="form-check">
+                        <input class="form-check-input" checked={checkMaxPoint} name="is_set_order_max_point" onChange={this.onChangeCheckBox} type="checkbox" id="gridCheck22" />
+                        <label class="form-check-label" for="gridCheck22">
+                          Xét giới hạn xu nhận được khi mua hàng
+                        </label>
+                      </div>
+
+                    </div>
+                    <div className={`form-group ${disableValueMaxPoint}`}>
+                      <label htmlFor="phone">Số xu nhận được tối đa khi mua hàng</label>
+                      <div className="group" style={{ display: "flex" }}>
+                        <div style={{ display: "flex" }}>
+                          <span style={{ margin: "auto", minWidth: "100px " }}>Số Xu: </span>
+
+                        </div>
+                        <div class="input-group" style={{ marginLeft: "20px", maxWidth: "100%" }}>
+
+                          <input type="text" class="form-control" onChange={this.onChange} value={formatNoDWithEmpty(order_max_point)} name="order_max_point" placeholder="Nhập số xu..." />
+                          <div class="input-group-append">
+                            <span class="input-group-text" id="basic-addon2">Xu</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div class="form-group">
+                      <div class="form-check">
+                        <input class="form-check-input" checked={isPercentUseMaxPoint} name="is_percent_use_max_point" onChange={this.onChangeCheckBox} type="checkbox" id="gridCheck" />
+                        <label class="form-check-label" for="gridCheck">
+                          Xét giới hạn số tiền có thể sử dụng bởi xu trong một đơn hàng
+                        </label>
+                      </div>
+
+                    </div>
+                    <div className={`form-group ${disablePercentUseMaxPont}`}>
+                      <label htmlFor="phone">Phần trăm số tiền tối đa sử dụng bằng xu trên mỗi đơn hàng</label>
+                      <div className="group" style={{ display: "flex" }}>
+                        <div style={{ display: "flex" }}>
+                          <span style={{ margin: "auto", minWidth: "100px " }}>Số phần trăm: </span>
+
+                        </div>
+                        <div class="input-group" style={{ marginLeft: "20px", maxWidth: "100%" }}>
+
+                          <input type="number" class="form-control" onChange={this.onChange} value={percent_use_max_point} name="percent_use_max_point" placeholder="Nhập số phần trăm..." />
+                          <div class="input-group-append">
+
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                }
+
+                <div className="form-group">
+                  <label htmlFor="phone">Xu cho đại lý</label>
+
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" checked={bonus_point_product_to_agency} name="bonus_point_product_to_agency" onChange={this.onChangeCheckBox} id="gridCheck333" />
+                    <label class="form-check-label" for="gridCheck333">
+                      Cho phép đại lý được hưởng xu sản phẩm
+                    </label>
+                  </div>
+
+                  {
+                    bonus_point_product_to_agency && <div class="form-check">
+                      <input class="form-check-input" type="checkbox" checked={bonus_point_bonus_product_to_agency} name="bonus_point_bonus_product_to_agency" onChange={this.onChangeCheckBox} id="gridCheck111" />
+                      <label class="form-check-label" for="gridCheck111">
+                        Cho phép tặng thêm xu từ sản phẩm thưởng
+                      </label>
+                    </div>
+                  }
+
+
                 </div>
 
-              </div>
-              {
-                checkedAllow == true && 
-                <>
-                       <div class="form-group">
-                  <div class="form-check">
-                    <input class="form-check-input" checked={checkMaxPoint} name="is_set_order_max_point" onChange={this.onChangeCheckBox} type="checkbox" id="gridCheck" />
-                    <label class="form-check-label" for="gridCheck">
-                      Xét giới hạn xu nhận được khi mua hàng
-                    </label>
-                  </div>
-  
-                </div>
-                  <div className={`form-group ${disableValueMaxPoint}`}>
-                  <label htmlFor="phone">Số xu nhận được tối đa khi mua hàng</label>
-                  <div className="group" style={{ display: "flex" }}>
-                    <div style={{ display: "flex" }}>
-                      <span style={{ margin: "auto", minWidth: "100px " }}>Số Xu: </span>
-  
-                    </div>
-                    <div class="input-group" style={{ marginLeft: "20px", maxWidth: "100%" }}>
-  
-                      <input type="text" class="form-control" onChange={this.onChange} value={formatNoDWithEmpty(order_max_point)} name="order_max_point" placeholder="Nhập số xu..." />
-                      <div class="input-group-append">
-                        <span class="input-group-text" id="basic-addon2">Xu</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-  
-  
-                <div class="form-group">
-                  <div class="form-check">
-                    <input class="form-check-input" checked={isPercentUseMaxPoint} name="is_percent_use_max_point" onChange={this.onChangeCheckBox} type="checkbox" id="gridCheck" />
-                    <label class="form-check-label" for="gridCheck">
-                      Xét giới hạn số tiền có thể sử dụng bởi xu trong một đơn hàng
-                    </label>
-                  </div>
-  
-                </div>
-                <div className={`form-group ${disablePercentUseMaxPont}`}>
-                  <label htmlFor="phone">Phần trăm số tiền tối đa sử dụng bằng xu trên mỗi đơn hàng</label>
-                  <div className="group" style={{ display: "flex" }}>
-                    <div style={{ display: "flex" }}>
-                      <span style={{ margin: "auto", minWidth: "100px " }}>Số phần trăm: </span>
-  
-                    </div>
-                    <div class="input-group" style={{ marginLeft: "20px", maxWidth: "100%" }}>
-  
-                      <input type="number" class="form-control" onChange={this.onChange} value={percent_use_max_point} name="percent_use_max_point" placeholder="Nhập số phần trăm..." />
-                      <div class="input-group-append">
-  
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                </>
-              }
-              
-  
-              
-                </React.Fragment>
-              
-            
+
+
+              </React.Fragment>
+
+
 
             </div>
 
