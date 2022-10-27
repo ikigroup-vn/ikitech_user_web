000ebb145fabb2cf7a95bf07e7d4866359e6cea6
Dr <drhoang999@gmail.com>
2022-10-27 14:28:20 +0700

    fix bug banner and updating choose interface

--- src/components/Theme/Home_Screen/ItemTheme.js
+++ src/components/Theme/Home_Screen/ItemTheme.js
@@ -3,25 +3,59 @@ import * as themeAction from "../../../actions/theme";
 import { connect } from "react-redux";
 import styled from "styled-components";
 
-
-
-
-const ItemThemeStyles = styled.div``
+const ItemThemeStyles = styled.div`
+  .card {
+    border: none;
+    box-shadow: 0 2px 10px 2px rgba(0, 0, 0, 0.07);
+    overflow: hidden;
+    .card__content {
+      img {
+        border-radius: 0% !important;
+      }
+      .theme__title {
+        width: 100%;
+        text-align: left;
+        padding: 10px;
+        position: relative;
+        border-top: 1px solid rgba(0, 0, 0, 0.07);
+        .theme__title-actived {
+          position: absolute;
+          padding: 0 10px;
+          top: 100%;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: #2c3e50;
+          color: white;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          transition: all 0.3s ease-in;
+          .theme__title__btnActive {
+            padding: 4px 10px;
+            background-color: #2980b9;
+            border-radius: 5px;
+          }
+        }
+      }
+    }
+  }
+`;
 
 class ItemTheme extends Component {
   constructor(props) {
     super(props);
     this.state = {
       home_page_type: null,
-      show_button: false
+      show_button: false,
     };
   }
 
   onChangeShowButton = (value) => {
     this.setState({
-      show_button: value
-    })
-  }
+      show_button: value,
+    });
+  };
 
   chooseTheme = (dataTheme) => {
     this.props.chooseTheme(dataTheme);
@@ -49,131 +83,141 @@ class ItemTheme extends Component {
     }
   };
 
+  handleShowDetailTheme = (infoDetails) => {
+    this.props.setShowModalDetailsTheme(true);
+    this.props.setInfoDetailsTheme(infoDetails);
+  };
   render() {
     var { home_page_type, v } = this.props;
-    var { show_button
-    } = this.state
+    var { show_button } = this.state;
     var isVip =
       typeof this.props.badges.config_user_vip == "undefined" ||
-        this.props.badges.config_user_vip == null
+      this.props.badges.config_user_vip == null
         ? false
         : true;
     var list_id_theme =
       typeof this.props.badges.config_user_vip == "undefined" ||
-        this.props.badges.config_user_vip != null
+      this.props.badges.config_user_vip != null
         ? this.props.badges.config_user_vip.list_id_theme_vip
         : [];
 
     return (
       <ItemThemeStyles
         onMouseEnter={() => {
-
-          this.onChangeShowButton(true)
+          this.onChangeShowButton(true);
         }}
         onMouseLeave={() => {
-
-          this.onChangeShowButton(false)
+          this.onChangeShowButton(false);
         }}
-        class={`form-group col-xs-3 col-lg-3 col-md-3 col-sm-3 ${this.checkExsitItem(v.index, v.isVip, isVip, list_id_theme) == true
-          ? ""
-          : "hide"
-          }`}
+        class={`form-group col-xs-3 col-lg-3 col-md-3 col-sm-3 ${
+          this.checkExsitItem(v.index, v.isVip, isVip, list_id_theme) == true
+            ? ""
+            : "hide"
+        }`}
       >
-
         <div class="card" style={{ padding: 0 }}>
-
-          <div style={{
-
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            position: "relative",
-            margin: "auto",
-            justifyContent: "center",
-            width: "100%"
-          }}>
+          <div
+            className="card__content"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              position: "relative",
+              margin: "auto",
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
             <img
               style={{ display: "block", objectFit: "cover" }}
               src={v.theme}
               width="100%"
               height={200}
-
               onMouseEnter={() => {
-
-                this.onChangeShowButton(true)
+                this.onChangeShowButton(true);
               }}
               onMouseLeave={() => {
-
-                this.onChangeShowButton(false)
+                this.onChangeShowButton(false);
               }}
-
             />
-
-
-
-            <div class="kv-avatar"
+            <div
+              class="kv-avatar"
               onMouseEnter={() => {
-
-                this.onChangeShowButton(true)
+                this.onChangeShowButton(true);
               }}
               onMouseLeave={() => {
-
-                this.onChangeShowButton(false)
+                this.onChangeShowButton(false);
               }}
-
               style={{
                 position: "absolute",
-
               }}
             >
-
-              {show_button == true &&
-
+              {show_button == true && (
                 <div style={{ display: "flex " }}>
                   <button
                     onClick={() => this.chooseTheme(v)}
                     style={{ margin: "10px auto" }}
                     type="button"
-                    class={`btn btn-primary btn-sm ${home_page_type !== v.index ? "show" : "hide"
-                      }`}
+                    class={`btn btn-primary btn-sm ${
+                      home_page_type !== v.index ? "show" : "hide"
+                    }`}
                   >
                     Chọn
                   </button>
                   &nbsp;&nbsp;
-                  <a href={v.demo_link} target="_blank">   <button
-
+                  <a href={v.demo_link} target="_blank">
+                    {" "}
+                    <button
+                      style={{ margin: "10px auto" }}
+                      type="button"
+                      class={`btn btn-primary btn-sm ${
+                        home_page_type !== v.index ? "show" : "hide"
+                      }`}
+                    >
+                      Xem demo
+                    </button>{" "}
+                  </a>
+                  &nbsp;&nbsp;
+                  <button
+                    onClick={() => this.handleShowDetailTheme(v)}
                     style={{ margin: "10px auto" }}
                     type="button"
-                    class={`btn btn-primary btn-sm ${home_page_type !== v.index ? "show" : "hide"
-                      }`}
+                    class={`btn btn-primary btn-sm ${
+                      home_page_type !== v.index ? "show" : "hide"
+                    }`}
                   >
-                    Xem demo
-                  </button> </a>
-
-
+                    Xem chi tiết
+                  </button>
                   <button
                     style={{ margin: "10px auto" }}
                     type="button"
-                    class={`btn btn-secondary btn-sm ${home_page_type === v.index ? "show" : "hide"
-                      }`}
+                    class={`btn btn-secondary btn-sm ${
+                      home_page_type === v.index ? "show" : "hide"
+                    }`}
                   >
                     <i class="fa fa-check"></i> Đã chọn
                   </button>
-
-
                 </div>
-
-              }
+              )}
             </div>
-              <p className="theme__title">
-                {v.name}
-              </p>
-            {home_page_type === v.index && <a onClick={this.props.goBack} class="ahef" style={{
-
-            }}><span>Tùy chỉnh</span></a>}
-
+            <div className="theme__title">
+              Giao diện: <span>{v.name}</span>
+              <div
+                className="theme__title-actived"
+                style={{
+                  top: home_page_type === v.index ? "0" : "100%",
+                }}
+              >
+                <span>Kích hoạt: {v.name}</span>
+                <a
+                  onClick={this.props.goBack}
+                  className="theme__title__btnActive"
+                >
+                  <span>Tùy chỉnh</span>
+                </a>
+              </div>
+            </div>
           </div>
-
         </div>
       </ItemThemeStyles>
     );
--- src/components/Theme/Home_Screen/index.js
+++ src/components/Theme/Home_Screen/index.js
@@ -6,12 +6,26 @@ import * as Env from "../../../ultis/default";
 import ItemTheme from "./ItemTheme";
 import Custom_Screen from "../Custom_Screen";
 import ModalDefaultReset from "./ModalDefaultReset";
+import styled from "styled-components";
+import ModalDetailsTheme from "./ModalDetailsTheme";
+
+const HomeScreenStyles = styled.div`
+  .theme__list {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    margin-top: 20px;
+  }
+`;
+
 class Home_Screen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       home_page_type: null,
       is_custom: false,
+      showDetailsTheme: false,
+      infoDetailsTheme: {},
     };
 
     //         1  => [1, 1, 1, 1, 1],
@@ -32,12 +46,13 @@ class Home_Screen extends Component {
           "https://data1.ikitech.vn/public/api/SahaImages/ZQLqkbDnCg1654823729.webp",
         arr_index_component: [1, 1, 1, 1, 1],
         demo_link: "https://mobiledemo.myiki.vn/",
-        name: 'Shop bán đồ điện tử',
-        banner_type:"Banner ngắn",
-        banner_height:"426px",
+        name: "Shop bán đồ điện tử",
+        banner_type: "Banner kết hợp danh mục và quảng cáo",
+        banner_height: "426px",
         banner_width: "743px",
-        description:"Giao diện giúp cho cửa hàng có thể dễ dàng bán các sản phẩm điện tử cho chính mình",
-        carrers:"Đồ điện tử, gia dụng"
+        description:
+          "Giao diện giúp cho cửa hàng có thể dễ dàng bán các sản phẩm điện tử cho chính mình",
+        carrers: "Đồ điện tử, gia dụng",
       },
       {
         index: 2,
@@ -45,11 +60,12 @@ class Home_Screen extends Component {
           "https://data1.ikitech.vn/public/api/SahaImages/UreKuxWypW1654823828.webp",
         arr_index_component: [2, 2, 2, 2, 1],
         demo_link: "https://shopmevabe.myiki.vn/",
-        name: 'Shop bán đồ trẻ em',
-        banner_type:"Banner ngắn",
-        banner_height:"410px",
+        name: "Shop bán đồ trẻ em",
+        banner_type: "Banner kết hợp danh mục",
+        banner_height: "410px",
         banner_width: "915px",
-        description:"Giao diện được thiết kế phù hợp trẻ trung, tươi mới phù hợp với bán các sản phẩm cho bé"
+        description:
+          "Giao diện được thiết kế phù hợp trẻ trung, tươi mới phù hợp với bán các sản phẩm cho bé",
       },
       {
         index: 3,
@@ -57,12 +73,13 @@ class Home_Screen extends Component {
           "https://data1.ikitech.vn/public/api/SahaImages/slSAdCmQzc1654823854.webp",
         arr_index_component: [3, 3, 3, 3, 1],
         demo_link: "https://noithatdemo.myiki.vn/",
-        name: 'Shop bán đồ nội thất',
-        banner_type:"Banner dài",
-        banner_height:"1530px",
+        name: "Shop bán đồ nội thất",
+        banner_type: "Banner trải dài",
+        banner_height: "1530px",
         banner_width: "580px",
-        description:"Giao diện được thiết kế phù hợp với bán các sản phẩm nội thát",
-        carrers:"Nội thất, Trang điểm"
+        description:
+          "Giao diện được thiết kế phù hợp với bán các sản phẩm nội thát",
+        carrers: "Nội thất, Trang điểm",
       },
       {
         index: 4,
@@ -70,12 +87,13 @@ class Home_Screen extends Component {
           "https://data1.ikitech.vn/public/api/SahaImages/uYJRbqpFYW1654823878.webp",
         arr_index_component: [4, 4, 4, 4, 1],
         demo_link: "https://myphamdemo.myiki.vn/",
-        name: 'Shop mỹ phẩm',
-        banner_type:"Banner trải dài",
-        banner_height:"1481px",
+        name: "Shop mỹ phẩm",
+        banner_type: "Banner trải dài",
+        banner_height: "1481px",
         banner_width: "475px",
-        description:"Giao diện được thiết kế trẻ trung, phong cách phù với bán các sản phẩm mỹ phẩm, thời trang",
-        carrers:"Thời trang, Mỹ phẩm"
+        description:
+          "Giao diện được thiết kế trẻ trung, phong cách phù với bán các sản phẩm mỹ phẩm, thời trang",
+        carrers: "Thời trang, Mỹ phẩm",
       },
       {
         isVip: false,
@@ -84,12 +102,13 @@ class Home_Screen extends Component {
           "https://data1.ikitech.vn/public/api/SahaImages/glDbUFhcrE1654823907.webp",
         arr_index_component: [5, 5, 5, 5, 2],
         demo_link: "https://thoitrangdemo.myiki.vn/",
-        name: 'Shop thời trang',
-        banner_type:"Banner ngắn",
-        banner_height:"410px",
+        name: "Shop thời trang",
+        banner_type: "Banner kết hợp danh mục và quảng cáo",
+        banner_height: "410px",
         banner_width: "673px",
-        description:"Giao diện được thiết kế trẻ trung, phong cách phù với bán các sản phẩm mỹ phẩm, thời trang",
-        carrers:"Thời trang, Trang điểm"
+        description:
+          "Giao diện được thiết kế trẻ trung, phong cách phù với bán các sản phẩm mỹ phẩm, thời trang",
+        carrers: "Thời trang, Trang điểm",
       },
       {
         isVip: false,
@@ -98,12 +117,13 @@ class Home_Screen extends Component {
           "https://data1.ikitech.vn/public/api/SahaImages/mIByceRszQ1654823953.webp",
         arr_index_component: [7, 6, 6, 3, 1],
         demo_link: "https://nowshopdemo.myiki.vn/",
-        name: 'Shop đồ ăn vặt',
-        banner_type:"Banner trải dài",
-        banner_height:"412px",
+        name: "Shop đồ ăn vặt",
+        banner_type: "Banner trải dài",
+        banner_height: "412px",
         banner_width: "1170px",
-        description:"Giao diện được thiết kế với gam màu rực rỡ phù hợp với bán các sản phẩm đồ ăn, hoa quả",
-        carrers:"Đồ ăn, hoa quả"
+        description:
+          "Giao diện được thiết kế với gam màu rực rỡ phù hợp với bán các sản phẩm đồ ăn, hoa quả",
+        carrers: "Đồ ăn, hoa quả",
       },
       {
         isVip: false,
@@ -112,12 +132,13 @@ class Home_Screen extends Component {
           "https://data1.ikitech.vn/public/api/SahaImages/GZZBFLEWFG1654823972.webp",
         arr_index_component: [8, 8, 7, 4, 2],
         demo_link: "https://dogiadungdemo.myiki.vn/",
-        name: 'Shop đồ gia dụng',
-        banner_type:"Banner ngắn",
-        banner_height:"410px",
+        name: "Shop đồ gia dụng",
+        banner_type: "Banner kết hợp danh mục và quảng cáo",
+        banner_height: "410px",
         banner_width: "685px",
-        description:"Giao diện được thiết kết phù hợp với các sản phẩm đồ gia dụng, đồ điện tử",
-        carrers:"Đồ gia dụng, đồ điện tử"
+        description:
+          "Giao diện được thiết kết phù hợp với các sản phẩm đồ gia dụng, đồ điện tử",
+        carrers: "Đồ gia dụng, đồ điện tử",
       },
       {
         isVip: false,
@@ -126,12 +147,13 @@ class Home_Screen extends Component {
           "https://data1.ikitech.vn/public/api/SahaImages/ERQPMELUMR1654823991.webp",
         arr_index_component: [9, 6, 5, 5, 5],
         demo_link: "https://shophoaquademo.myiki.vn/",
-        name: 'Shop hoa quả',
-        banner_type:"Banner trải dài",
-        banner_height:"1481px",
+        name: "Shop hoa quả",
+        banner_type: "Banner trải dài",
+        banner_height: "1481px",
         banner_width: "440px",
-        description:"Giao diện được thiết kế phù hợp với bán các mặt hàng đồ ăn, hoa quả",
-        carrers:"Đồ ăn, hoa quả"
+        description:
+          "Giao diện được thiết kế phù hợp với bán các mặt hàng đồ ăn, hoa quả",
+        carrers: "Đồ ăn, hoa quả",
       },
       {
         isVip: false,
@@ -139,28 +161,41 @@ class Home_Screen extends Component {
         theme: "https://i.imgur.com/lTwsZhY.png",
         arr_index_component: [10, 9, 9, 2, 5],
         demo_link: "https://thucphamsach.myiki.vn/",
-        name: 'Shop thực phẩm',
-        banner_type:"Banner ngắn",
-        banner_height:"355px",
+        name: "Shop thực phẩm",
+        banner_type: "Banner kết hợp danh mục và quảng cáo",
+        banner_height: "355px",
         banner_width: "702px",
-        description:"Giao diện phù hợp với bán các sản phẩm liên quan đến đồ ăn, hoa quả",
-        carrers:"Đồ ăn, hoa quả"
+        description:
+          "Giao diện phù hợp với bán các sản phẩm liên quan đến đồ ăn, hoa quả",
+        carrers: "Đồ ăn, hoa quả",
       },
       {
         isVip: false,
         index: 10,
-        theme: "https://www.cgv.vn/media/catalog/product/cache/1/image/c5f0a1eff4c394a251036189ccddaacd/p/o/poster_adam_4_1.jpg",
+        theme:
+          "https://www.cgv.vn/media/catalog/product/cache/1/image/c5f0a1eff4c394a251036189ccddaacd/p/o/poster_adam_4_1.jpg",
         arr_index_component: [11, 10, 10, 8, 6],
         demo_link: "https://thucphamsach.myiki.vn/",
-        name: 'Shop thời trang 2',
-        banner_type:"Banner trải dài",
-        banner_height:"677px",
+        name: "Shop thời trang 2",
+        banner_type: "Banner trải dài",
+        banner_height: "677px",
         banner_width: "1481px",
-        description:"Giao diện được thiết kế trẻ trung, phong cách phù với bán các sản phẩm mỹ phẩm, thời trang",
-        carrers:"Thời trang, trang điểm"
+        description:
+          "Giao diện được thiết kế trẻ trung, phong cách phù với bán các sản phẩm mỹ phẩm, thời trang",
+        carrers: "Thời trang, trang điểm",
       },
     ];
   }
+  setShowModalDetailsTheme = (showDetails) => {
+    this.setState({
+      showDetailsTheme: showDetails,
+    });
+  };
+  setInfoDetailsTheme = (infoDetails) => {
+    this.setState({
+      infoDetailsTheme: infoDetails,
+    });
+  };
   getThemeDefault = (index) => {
     var data = null;
     this.initTheme.forEach((e) => {
@@ -257,7 +292,7 @@ class Home_Screen extends Component {
     if (is_custom == true) {
       return (
         <Custom_Screen
-          // tabId={tabId}
+          // tabId={tabId}\dashboard\hahaha
           theme_default={this.state.theme_default}
           store_code={store_code}
           chooseTheme={this.chooseTheme}
@@ -271,29 +306,36 @@ class Home_Screen extends Component {
     }
 
     return (
-      <div className="overview">
+      <HomeScreenStyles className="overview">
         <form role="form">
           <div class="box-body">
-            <div>
-              <div class="row">
-                {this.initTheme.map((v, i) => (
-                  <ItemTheme
-                    key={i}
-                    badges={badges}
-                    chooseTheme={this.chooseTheme}
-                    home_page_type={home_page_type}
-                    v={v}
-                    goBack={() => {
-                      this.onChangeCustom();
-                    }}
-                  />
-                ))}
-              </div>
+            <div class="theme__list">
+              {this.initTheme.map((v, i) => (
+                <ItemTheme
+                  key={i}
+                  badges={badges}
+                  chooseTheme={this.chooseTheme}
+                  home_page_type={home_page_type}
+                  v={v}
+                  goBack={() => {
+                    this.onChangeCustom();
+                  }}
+                  setShowModalDetailsTheme={this.setShowModalDetailsTheme}
+                  setInfoDetailsTheme={this.setInfoDetailsTheme}
+                />
+              ))}
             </div>
+            {Object.entries(this.state.infoDetailsTheme).length > 0 &&
+              this.state.showDetailsTheme && (
+                <ModalDetailsTheme
+                  themeInfo={this.state.infoDetailsTheme}
+                  setShowModalDetailsTheme={this.setShowModalDetailsTheme}
+                />
+              )}
           </div>
           <div class="box-footer"></div>
         </form>
-      </div>
+      </HomeScreenStyles>
     );
   }
 }
