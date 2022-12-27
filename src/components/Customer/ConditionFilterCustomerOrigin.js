a9f852ceb2907856f217cc793a5f21ed5a9f04c6
Dr <drhoang999@gmail.com>
2022-12-27 17:25:28 +0700

    update search filter

--- src/components/Customer/ConditionFilterCustomerOrigin.js
+++ src/components/Customer/ConditionFilterCustomerOrigin.js
@@ -8,15 +8,14 @@ const ConditionFilterCustomerStyles = styled.div`
   display: flex;
   column-gap: 20px;
   max-height: 38px;
-
   .sidebar__conditions__dropdown__expression {
     height: 100%;
     width: 80px;
     flex-shrink: 0;
     select {
-      border: 1px solid transparent;
-      border-bottom-color: #d1d3e2;
+      border: 1px solid #d1d3e2;
       padding: 0.375rem 0.75rem;
+      border-radius: 0.35rem;
       width: 100%;
       outline: none;
       height: 38px;
@@ -27,9 +26,9 @@ const ConditionFilterCustomerStyles = styled.div`
     width: 180px;
     & > div {
       display: flex;
-      border: 1px solid transparent;
-      border-bottom-color: #d1d3e2;
+      border: 1px solid #d1d3e2;
       padding: 0.375rem 0.75rem;
+      border-radius: 0.35rem;
       height: 38px;
 
       input {
@@ -47,9 +46,9 @@ const ConditionFilterCustomerStyles = styled.div`
     }
     select {
       width: 100%;
-      border: 1px solid transparent;
-      border-bottom-color: #d1d3e2;
+      border: 1px solid #d1d3e2;
       padding: 0.375rem 0.75rem;
+      border-radius: 0.35rem;
       height: 38px;
       outline: none;
     }
--- src/constants/ActionType.js
+++ src/constants/ActionType.js
@@ -343,6 +343,7 @@ export const TYPE_COMPARE_PROVINCE = 7; // tỉnh,
 export const TYPE_COMPARE_DATE_REG = 8; // ngày đăng ký
 export const TYPE_COMPARE_CTV = 9; // cộng tác viên
 export const TYPE_COMPARE_AGENCY = 10; // Đại lý
+export const TYPE_COMPARE_GROUP_CUSTOMER = 11; // Theo nhóm khách hàng
 
 // HISTORY OPERATION
 export const GET_HISTORY_OPERATION = "GET_HISTORY_OPERATION";
