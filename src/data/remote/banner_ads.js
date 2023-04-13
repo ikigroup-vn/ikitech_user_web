990c44317e132465ab1e897c269582e69fe4d2b8
HoangDr <drhoang999@gmail.com>
2023-04-13 10:52:20 +0700

    Merge huy

--- src/data/remote/banner_ads.js
+++ src/data/remote/banner_ads.js
@@ -1,20 +1,25 @@
 import callApi from "../../ultis/apiCaller";
 
 export const fetchAllData = (store_code) => {
-  return callApi(`/store/${store_code}/banner_ads`, "get", null);
+  return callApi(`/store/${store_code}/banner_webad`, "get", null);
 };
 
+export const createBannerAds = (store_code, data) => {
+  return callApi(`/store/${store_code}/banner_webad`, "post", data);
+};
 
+export const updateBannerAds = (bannerAdsId, categoryB, store_code) => {
+  return callApi(
+    `/store/${store_code}/banner_webad/${bannerAdsId}`,
+    "put",
+    categoryB
+  );
+};
 
-export const createBannerAds = (store_code,data) =>{
-  return callApi(`/store/${store_code}/banner_ads`, "post", data);
-}
-
-export const updateBannerAds = (bannerAdsId, categoryB, store_code) =>{
-  console.log("vao222" , bannerAdsId, categoryB, store_code)
-  return callApi(`/store/${store_code}/banner_ads/${bannerAdsId}`, "put", categoryB);
-}
-
-export const destroyBannerAds = (store_code , bannerAdsId) =>{
-  return callApi(`/store/${store_code}/banner_ads/${bannerAdsId}`, "delete", null);
-}
+export const destroyBannerAds = (store_code, bannerAdsId) => {
+  return callApi(
+    `/store/${store_code}/banner_webad/${bannerAdsId}`,
+    "delete",
+    null
+  );
+};
