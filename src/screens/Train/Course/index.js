e59473d8925ba66ecfe917513b562a8fd2c7fd9a
Huy Nguyễn Thạc <huyntdev@gmail.com>
2023-09-25 00:26:40 +0700

    Update permission train, communication and fixbug schedule group_customer

--- src/screens/Train/Course/index.js
+++ src/screens/Train/Course/index.js
@@ -21,25 +21,22 @@ class Course extends Component {
         title: "",
         id: "",
         name: "",
-
       },
-      numPage: 20
-
+      numPage: 20,
     };
   }
 
   onChangeNumPage = (e) => {
     var { store_code } = this.props.match.params;
     // var {searchValue} = this.state
-    var numPage = e.target.value
+    var numPage = e.target.value;
     this.setState({
-      numPage
-    })
-    var params = `&limit=${numPage}`
+      numPage,
+    });
+    var params = `&limit=${numPage}`;
 
     this.props.fetchAllCourse(store_code, 1, params);
-  }
-
+  };
 
   handleDelCallBack = (modal) => {
     this.setState({ modal: modal });
@@ -49,13 +46,26 @@ class Course extends Component {
     this.props.fetchAllCourse(this.props.match.params.store_code);
   }
   componentWillReceiveProps(nextProps) {
-    if (this.state.isLoading != true && typeof nextProps.permission.product_list != "undefined") {
-      var permissions = nextProps.permission
+    if (
+      this.state.isLoading != true &&
+      typeof nextProps.permission.product_list != "undefined"
+    ) {
+      var permissions = nextProps.permission;
       // var insert = permissions.post_add
       // var update = permissions.post_update
       // var _delete = permissions.post_remove_hide
-      var isShow = permissions.train
-      this.setState({ isLoading: true, insert:true, update:true, _delete:true, isShow })
+      var isShow = permissions.train;
+      var train_add = permissions.train_add;
+      var train_update = permissions.train_update;
+      var train_delete = permissions.train_delete;
+
+      this.setState({
+        isLoading: true,
+        insert: train_add,
+        update: train_update,
+        _delete: train_delete,
+        isShow,
+      });
     }
   }
   onChangeSearch = (e) => {
@@ -72,10 +82,10 @@ class Course extends Component {
   };
 
   render() {
-    var { store_code } = this.props.match.params
-    var { courses } = this.props
-    var { numPage,searchValue } = this.state
-    var { insert, update, _delete, isShow } = this.state
+    var { store_code } = this.props.match.params;
+    var { courses } = this.props;
+    var { numPage, searchValue } = this.state;
+    var { insert, update, _delete, isShow } = this.state;
 
     if (this.props.auth) {
       return (
@@ -85,116 +95,123 @@ class Course extends Component {
             <div id="content-wrapper" className="d-flex flex-column">
               <div id="content">
                 <Topbar store_code={store_code} />
-                {typeof isShow == "undefined" ? <div style={{ height: "500px" }}></div> :
-                  isShow == true ?
-
-                    <div className="container-fluid">
-
-
-                      <Alert
-                        type={Types.ALERT_UID_STATUS}
-                        alert={this.props.alert}
-                      />
-                      <div
-                        style={{ display: "flex", justifyContent: "space-between" }}
-                      >
-                        <h4 className="h4 title_content mb-0 text-gray-800">
+                {typeof isShow == "undefined" ? (
+                  <div style={{ height: "500px" }}></div>
+                ) : isShow == true ? (
+                  <div className="container-fluid">
+                    <Alert
+                      type={Types.ALERT_UID_STATUS}
+                      alert={this.props.alert}
+                    />
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <h4 className="h4 title_content mb-0 text-gray-800">
                         Đào tạo - Khóa học
-                                                </h4>{" "}
-                        <Link to={`/train/course/create/${store_code}`}
-                          class={`btn btn-info btn-icon-split btn-sm ${insert == true ? "show" : "hide"}`}
+                      </h4>{" "}
+                      <Link
+                        to={`/train/course/create/${store_code}`}
+                        class={`btn btn-info btn-icon-split btn-sm ${
+                          insert == true ? "show" : "hide"
+                        }`}
+                      >
+                        <span class="icon text-white-50">
+                          <i class="fas fa-plus"></i>
+                        </span>
+                        <span class="text">Thêm khóa học</span>
+                      </Link>
+                    </div>
+
+                    <br></br>
+                    <div className="card shadow ">
+                      <div className="card-header">
+                        <div
+                          class="row"
+                          style={{ "justify-content": "space-between" }}
                         >
-                          <span class="icon text-white-50">
-                            <i class="fas fa-plus"></i>
-                          </span>
-                          <span class="text">Thêm khóa học</span>
-                        </Link>
-                      </div>
-
-                      <br></br>
-                      <div className="card shadow ">
-                        <div className="card-header">
-                          <div class="row" style={{ "justify-content": "space-between" }}>
-                            <form onSubmit={this.searchData}>
-                              <div
-                                class="input-group mb-6"
-                                style={{ padding: "0 20px" }}
-                              >
-                                <input
-                                  style={{ maxWidth: "400px", minWidth: "300px" }}
-                                  type="search"
-                                  name="txtSearch"
-                                  value={searchValue}
-                                  onChange={this.onChangeSearch}
-                                  class="form-control"
-                                  placeholder="Tìm theo khóa học"
-                                />
-                                <div class="input-group-append">
-                                  <button
-                                    class="btn btn-primary"
-                                    type="submit"
-
-                                  >
-                                    <i class="fa fa-search"></i>
-                                  </button>
-                                </div>
-
+                          <form onSubmit={this.searchData}>
+                            <div
+                              class="input-group mb-6"
+                              style={{ padding: "0 20px" }}
+                            >
+                              <input
+                                style={{ maxWidth: "400px", minWidth: "300px" }}
+                                type="search"
+                                name="txtSearch"
+                                value={searchValue}
+                                onChange={this.onChangeSearch}
+                                class="form-control"
+                                placeholder="Tìm theo khóa học"
+                              />
+                              <div class="input-group-append">
+                                <button class="btn btn-primary" type="submit">
+                                  <i class="fa fa-search"></i>
+                                </button>
                               </div>
-                              {/* <p class="total-item" id="sale_user_name">
+                            </div>
+                            {/* <p class="total-item" id="sale_user_name">
                                 <span className="num-total_item" >{products.total}&nbsp;</span><span className="text-total_item" id="user_name">sản phẩm</span>
                               </p> */}
-                            </form>
+                          </form>
+                          <div style={{ display: "flex" }}>
                             <div style={{ display: "flex" }}>
-
-                              <div style={{ display: "flex" }}>
-                                <span
-                                  style={{
-                                    margin: "20px 10px auto auto"
-                                  }}
-                                >Hiển thị</span>
-                                <select
-                                  style={{
-                                    margin: "auto",
-                                    marginTop: "10px",
-                                    marginRight: "20px",
-                                    width: "70px",
-                                  }}
-                                  onChange={this.onChangeNumPage}
-
-                                  value={numPage}
-                                  name="numPage" class="form-control" >
-                                  <option value="10">10</option>
-                                  <option value="20" selected>20</option>
-                                  <option value="50">50</option>
-                                </select>
-                              </div>
-
-
-                              {/* <Pagination limit={numPage}
-                        searchValue = {searchValue}
-                          passNumPage={this.passNumPage} store_code={store_code} products={products} /> */}
-
-
+                              <span
+                                style={{
+                                  margin: "20px 10px auto auto",
+                                }}
+                              >
+                                Hiển thị
+                              </span>
+                              <select
+                                style={{
+                                  margin: "auto",
+                                  marginTop: "10px",
+                                  marginRight: "20px",
+                                  width: "70px",
+                                }}
+                                onChange={this.onChangeNumPage}
+                                value={numPage}
+                                name="numPage"
+                                class="form-control"
+                              >
+                                <option value="10">10</option>
+                                <option value="20" selected>
+                                  20
+                                </option>
+                                <option value="50">50</option>
+                              </select>
                             </div>
 
-
+                            {/* <Pagination limit={numPage}
+                        searchValue = {searchValue}
+                          passNumPage={this.passNumPage} store_code={store_code} products={products} /> */}
                           </div>
                         </div>
+                      </div>
 
-                        <div className="card-body">
-                          <Table update={update} _delete={_delete} store_code={store_code} handleDelCallBack={this.handleDelCallBack} courses={courses} />
+                      <div className="card-body">
+                        <Table
+                          update={update}
+                          _delete={_delete}
+                          store_code={store_code}
+                          handleDelCallBack={this.handleDelCallBack}
+                          courses={courses}
+                        />
 
-                          <Pagination
-                            limit={numPage}
-
-                            store_code={store_code}
-                            courses={courses}
-                          />
-                        </div>
+                        <Pagination
+                          limit={numPage}
+                          store_code={store_code}
+                          courses={courses}
+                        />
                       </div>
                     </div>
-                    : <NotAccess />}
-
+                  </div>
+                ) : (
+                  <NotAccess />
+                )}
               </div>
 
               <Footer />
@@ -202,7 +219,6 @@ class Course extends Component {
             <ModalDelete store_code={store_code} modal={this.state.modal} />
           </div>
         </div>
-
       );
     } else if (this.props.auth === false) {
       return <Redirect to="/login" />;
@@ -217,7 +233,6 @@ const mapStateToProps = (state) => {
     courses: state.trainReducers.train.allCourse,
     auth: state.authReducers.login.authentication,
     permission: state.authReducers.permission.data,
-
   };
 };
 const mapDispatchToProps = (dispatch, props) => {
