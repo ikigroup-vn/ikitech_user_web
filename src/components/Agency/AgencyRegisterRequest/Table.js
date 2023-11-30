7a492ba253bfec870613e1dea538cd06ad5aa2b5
Huy Nguyễn Thạc <huyntdev@gmail.com>
2023-11-30 16:59:52 +0700

    Fixbug request agency error

--- src/components/Agency/AgencyRegisterRequest/Table.js
+++ src/components/Agency/AgencyRegisterRequest/Table.js
@@ -165,15 +165,15 @@ class Table extends Component {
     var result = null;
     if (agencyRegisterRequests.length > 0) {
       result = agencyRegisterRequests.map((data2, index) => {
-        var data = data2.agency;
+        var data = data2?.agency;
         var avatar =
           data?.customer?.avatar_image == null
             ? Env.IMG_NOT_FOUND
             : data?.customer?.avatar_image;
         var img_front =
-          data.front_card == null ? Env.IMG_NOT_FOUND : data.front_card;
+          data?.front_card == null ? Env.IMG_NOT_FOUND : data?.front_card;
         var img_back =
-          data.back_card == null ? Env.IMG_NOT_FOUND : data.back_card;
+          data?.back_card == null ? Env.IMG_NOT_FOUND : data?.back_card;
 
         var address_default = "";
 
@@ -225,7 +225,7 @@ class Table extends Component {
                   1}
               </td>{" "}
               <td>{data?.customer?.name}</td>
-              <td>{data.account_name}</td>
+              <td>{data?.account_name}</td>
               <td>{data?.customer?.phone_number}</td>
               <td>
                 {data?.customer?.total_after_discount_no_bonus
@@ -287,30 +287,30 @@ class Table extends Component {
                       <p class="sale_user_label">
                         Số tài khoản:{" "}
                         <span id="user_tel">
-                          {data.account_number} - {data.bank}{" "}
+                          {data?.account_number} - {data?.bank}{" "}
                         </span>
                       </p>
                       <p class="sale_user_label">
                         Tên chủ tài khoản:{" "}
-                        <span id="user_tel">{data.account_name}</span>
+                        <span id="user_tel">{data?.account_name}</span>
                       </p>
 
                       <p class="sale_user_label">
                         Tên CMND:{" "}
-                        <span id="user_tel">{data.first_and_last_name}</span>
+                        <span id="user_tel">{data?.first_and_last_name}</span>
                       </p>
 
                       <p class="sale_user_label" id="sale_user_name">
-                        CMND: <span id="user_name"> {data.cmnd} </span>
+                        CMND: <span id="user_name"> {data?.cmnd} </span>
                       </p>
                       <p class="sale_user_label" id="sale_user_name">
                         Nơi đăng kí:{" "}
-                        <span id="user_name"> {data.issued_by} </span>
+                        <span id="user_name"> {data?.issued_by} </span>
                       </p>
                       <p class="sale_user_label" id="sale_user_name">
                         Ngày đăng ký CTV:{" "}
                         <span id="user_name">
-                          {moment(data.created_at).format("DD-MM-YYYY")}{" "}
+                          {moment(data?.created_at).format("DD-MM-YYYY")}{" "}
                         </span>
                       </p>
                       {/* {address_default !== "" && (
