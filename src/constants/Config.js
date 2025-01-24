85ed9a7d5886ec520119a1afa9861d7c6d456bdb
Nghia <nghia@Nghias-MacBook-Air.local>
2025-01-24 14:09:38 +0700

    change_config

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -3,11 +3,11 @@ import { getBranch } from "../ultis/branchUtils";
 
 // export const API_URL  = 'https://doshop.sahavi.vn/api';
 // export const API_URL_SOCKET = "https://doshop.sahavi.vn:6441/"
-export const API_URL_DEV = "https://main.doapp.vn/api";
+export const API_URL_DEV = "https://snetapi.ikitech.vn/api";
 // export const API_URL_DEV = "http://127.0.0.1:8000/api";
-export const API_URL_MAIN = "https://main.doapp.vn/api";
-export const API_URL_SOCKET_DEV = "https://main.doapp.vn:6441/";
-export const API_URL_SOCKET_MAIN = "https://main.doapp.vn:6441/";
+export const API_URL_MAIN = "https://snetapi.ikitech.vn/api";
+export const API_URL_SOCKET_DEV = "https://snetapi.ikitech.vn:6441/";
+export const API_URL_SOCKET_MAIN = "https://snetapi.ikitech.vn:6441/";
 export const getApiImageStore = (store_code) => {
   return `${helpers.callUrl()}/store/${store_code}/images`;
 };
