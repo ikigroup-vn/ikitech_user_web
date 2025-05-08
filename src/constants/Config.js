307856f5fe779295e09de2b3294ccbca632813cb
ThangPham <thang.ph@ikigroup.vn>
2025-05-08 14:59:00 +0700

    update(domain): socket and api

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -3,12 +3,12 @@ import { getBranch } from "../ultis/branchUtils";
 
 // export const API_URL  = 'https://doshop.sahavi.vn/api';
 // export const API_URL_SOCKET = "https://doshop.sahavi.vn:6441/"
-export const API_URL_DEV = "https://main.dongtien.ikitech.vn/api";
+export const API_URL_DEV = "https://apibanhang.dongtienbakery.com/api";
 
 // export const API_URL_DEV = "http://127.0.0.1:8000/api";
-export const API_URL_MAIN = "https://main.dongtien.ikitech.vn/api";
-export const API_URL_SOCKET_DEV = "https://main.doapp.vn:6441/";
-export const API_URL_SOCKET_MAIN = "https://main.doapp.vn:6441/";
+export const API_URL_MAIN = "https://apibanhang.dongtienbakery.com/api";
+export const API_URL_SOCKET_DEV = "https://apibanhang.dongtienbakery.com:3333/";
+export const API_URL_SOCKET_MAIN = "https://apibanhang.dongtienbakery.com:3333/";
 export const getApiImageStore = (store_code) => {
   return `${helpers.callUrl()}/store/${store_code}/images`;
 };
